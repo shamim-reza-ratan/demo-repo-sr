9ef4cdf8d64f7cd36696762c230affe6031404d7
shamim-reza-ratan <shamim.reza.ratan@gmail.com>
2025-01-04 19:02:46 -0500

    updated twoSum script with driver code

--- twoSum.py
+++ twoSum.py
@@ -1,10 +1,15 @@
 from typing import List
 
-def two_sum(input_list: List(int), target: int) -> List(int):
+def two_sum(input_list: List[int], target: int) -> List[int]:
   prev_map = {}
   for i, val in enumerate(input_list):
     diff_val = target - val
     if diff_val in prev_map:
       return [prev_map[diff_val], i]
-else:
-prev_map[val] = i
+    
+    prev_map[val] = i
+
+arr = [2, 5, 7, 11, 9]
+target = 21
+
+print(two_sum(arr, target))
